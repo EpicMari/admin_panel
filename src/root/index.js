ae857528fcefa2f066d59143c15313b057474146
Martin Reinicke <reinicke.mari@gmail.com>
2021-11-08 17:21:42 +0100

    Add listening on firestore database

--- src/root/index.js
+++ src/root/index.js
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { ThemeProvider } from "styled-components";
+import { ordersCollection } from "../firebase/firestoreUtils";
 import { GlobalStyle } from "../globalStyle/GlobalStyle";
 import Router from "../routing";
 import { themes } from "../themes/themes";
+import { useDispatch } from "react-redux";
+import { setOrders } from "../redux/actions";
 
 const Root = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const subscribe = ordersCollection.onSnapshot((snapshot) => {
+      const dataFromOrdersCollection = snapshot.docs.map((doc) => {
+        return {
+          docId: doc.id,
+          ...doc.data(),
+        };
+      });
+
+      console.log(dataFromOrdersCollection);
+
+      dispatch(setOrders(dataFromOrdersCollection));
+    });
+
+    return () => {
+      subscribe();
+    };
+  }, []);
+
   return (
     <ThemeProvider theme={themes}>
       <GlobalStyle />
