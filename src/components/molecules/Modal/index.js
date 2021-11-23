f80dceb78a20fce88c52417e8b7b49966696536d
Martin Reinicke <reinicke.mari@gmail.com>
2021-11-23 17:05:08 +0100

    Refactor Modal component

--- src/components/molecules/Modal/index.js
+++ src/components/molecules/Modal/index.js
@@ -1,44 +1,51 @@
-import * as React from "react";
+import React from "react";
+import { Button, Modal as ModalMui } from "@mui/material";
 import Box from "@mui/material/Box";
-import IconButton from "@mui/material/IconButton";
-import Modal from "@mui/material/Modal";
-import EditIcon from "@mui/icons-material/Edit";
-import Paragraph from "../../atoms/Paragraph";
+import { useDispatch, useSelector } from "react-redux";
+import { deleteOrders, toggleModalOpen } from "../../../redux/actions";
+import utilsReducer from "../../../redux/reducers/utilsReducer";
 
-const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 400,
-  bgcolor: "background.paper",
-  border: "2px solid #000",
-  boxShadow: 24,
-  p: 4,
-};
-
-const BasicModal = ({ row: { status } }) => {
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+const Modal = ({ numSelected }) => {
+  const dispatch = useDispatch();
+  const isOpenModal = useSelector(
+    ({ utilsReducer }) => utilsReducer.isOpenModal
+  );
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    boxShadow: 24,
+    p: 4,
+  };
 
   return (
-    <>
-      <IconButton onClick={handleOpen}>
-        <EditIcon />
-      </IconButton>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <Paragraph>{status}</Paragraph>
-        </Box>
-      </Modal>
-    </>
+    <ModalMui
+      open={isOpenModal}
+      onClose={() => dispatch(toggleModalOpen())}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box sx={style}>
+        <Button
+          color="error"
+          variant="contained"
+          onClick={() => dispatch(deleteOrders(numSelected))}
+        >
+          Delete
+        </Button>
+        <Button
+          color="primary"
+          variant="contained"
+          onClick={() => dispatch(toggleModalOpen())}
+        >
+          Cancel
+        </Button>
+      </Box>
+    </ModalMui>
   );
 };
 
-export default BasicModal;
+export default Modal;
