af4c36e0e8b533c7d9ef1e989f9406e6e502181a
Martin Reinicke <reinicke.mari@gmail.com>
2021-11-23 17:11:57 +0100

    Add delete fn from firestore

--- src/firebase/firestoreUtils.js
+++ src/firebase/firestoreUtils.js
@@ -2,3 +2,9 @@ import { firestore } from "./firebaseConfig";
 
 export const usersCollection = firestore.collection("adminPanelUsers");
 export const ordersCollection = firestore.collection("orders");
+
+export const deleteOrderFromFirestore = (ordersToDelete) => {
+  ordersToDelete.forEach((orderId) => {
+    ordersCollection.doc(orderId).delete();
+  });
+};
